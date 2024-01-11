db363b2c42f8a7720b2b2a023c19551ef0b5681b
Christoffer Jansson <christofferjansson1@gmail.com>
2024-01-11 18:22:30 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBinding.Models;
+using System.Diagnostics;
+
+namespace ModelBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Human human = new Human();
+
+            human.Name = "Bob";
+            human.Age = 30;
+            human.Handsome = true;
+
+            return View(human);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Human.cs
@@ -0,0 +1,9 @@
+﻿namespace ModelBinding.Models
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool Handsome { get; set; }
+    }
+}
